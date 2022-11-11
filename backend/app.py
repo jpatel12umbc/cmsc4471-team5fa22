0423df741588d0aaf4ae671b40946670ec200f3f
Dillon Ward <98842483+Dillon1271@users.noreply.github.com>
2022-11-11 15:50:06 -0500

    Covid Line Graph

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-#import pymysql
-#pymysql.install_as_MySQLdb()
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+import pymysql
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:PASSWORD@localhost:3306/DATABASENAME'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost:3306/447_groupproj'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+db.init_app(app)
+ma = Marshmallow(app)
+CORS(app)
+
 
 #creates crimecode table
 class crimecode(db.Model):
@@ -18,7 +24,7 @@ class crimecode(db.Model):
     def __init__(self, CODE, CrimeName):
         self.CODE = CODE
         self.CrimeName = CrimeName
-     
+
 #creates covidcase table   
 class covidcase(db.Model):
  
@@ -35,6 +41,12 @@ class covidcase(db.Model):
         self.DailyIncrease = DailyIncrease
         self.AVGIncrease = AVGIncrease
         
+
+class CovidSchema(ma.Schema):
+    class Meta:
+        fields = ('DayNum', 'Date', 'TotalCases', 'DailyIncrease', 'AVGIncrease')
+
+covid_schema = CovidSchema(many=True)
 
 #creates crimedata table       
 class crimedata(db.Model):
@@ -65,13 +77,72 @@ def parse_geodata(GeoLocation):
     GeoLocation = [float(x) for x in GeoLocation]
     return GeoLocation
 
+
 ######initial route for display all data. i.e graphs heatmap etc.###############        
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     all_covid = covidcase.query.all()
-    all_crime = crimedata.query.all()
-    return render_template()
+    #query = covid_schema.dump(all_covid)
+    #all_crime = crimedata.query.all()
 
+    #testing
+    rollingavg = []
+    #for x in all_covid:
+    i = 0
+    while i < len(all_covid): 
+        dict = {"x": all_covid[i].DATE, "y": all_covid[i].AVGIncrease}
+        #dict = {"x": i, "y": all_covid[i].AVGIncrease}
+        rollingavg.append(dict)
+        i+=1
+
+    return jsonify(rollingavg)
+    #return jsonify(query)
+    
+    
+#Line graph for covid 1-week average
+@app.route('/covidlinegraph', methods=['GET'])
+def dateslist():
+
+    #user's input from frontend filter
+    sdate = str(request.args.get("startdate"))
+    edate = (request.args.get("enddate"))
+
+    all_covid = covidcase.query.all()
+    #query = covid_schema.dump(all_covid)
+
+
+    #converting user input from html calendar to DATETIME format to match query
+    sdate = sdate.replace("-", "/")
+    sdate += " 10:00:00+00"
+    edate = edate.replace("-", "/")
+    edate += " 10:00:00+00"
+
+    startd = covidcase.query.filter_by(DATE = sdate).first()
+    endd = covidcase.query.filter_by(DATE = edate).first()
+
+    #print("test!", sdate , " " , startd.DayNum , " " , endd.DayNum)
+
+    startDayNum = int(startd.DayNum)
+    endDayNum = int(endd.DayNum)
+
+
+    rollingavg = []
+    
+    i = startDayNum
+    while i <= endDayNum:
+        #adds dictionaries to the list, for x and y coordinate of each point on the graph (x as DATETIME format)
+        dict = {"x": (all_covid[i-1].DATE)[0:10], "y": all_covid[i-1].AVGIncrease}
+
+        #adds dictionaries to the list, for x and y coordinate of each point on the graph (x as DayNum int format)
+        #dict = {"x": i, "y": all_covid[i].AVGIncrease}
+        rollingavg.append(dict)
+        i+=1
+
+    #returning value will override the coviddata variable (in App.js) to update the graph
+    return jsonify(rollingavg)
+    
+
+###############################################################################################################################
 
 ####update for when user changes a startdate, end date and or location for covid data##########
 # @app.route('/update', methods = ['GET', 'POST'])
