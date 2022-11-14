63eb3b06c77b6dd9764871ed5d6d1ca73d24d2e0
Kevin-Mens <79649505+Kevin-Mens@users.noreply.github.com>
2022-11-14 11:58:02 -0500

    Add files via upload

---
+++ DDLCrimeCovid3.0.sql
@@ -0,0 +1,42 @@
+drop database if exists CrimeCovid;
+create database if not exists CrimeCovid;
+use CrimeCovid;
+
+drop table if exists `CrimeCode`;
+create table if not exists `CrimeCode`(
+	`Code` varchar(4),
+	`CrimeName` varchar(25),
+	primary key (`Code`)
+);
+
+drop table if exists `Weapon`;
+create table if not exists `Weapon`(
+	`WeaponID` tinyint,
+	`WeaponName` varchar(30),
+	primary key (`WeaponID`)
+);
+
+drop table if exists `CovidCase`;
+create table if not exists `CovidCase`(
+	`DayNum` smallint,
+	`Date` date,
+	`TotalCases` mediumint,
+	`DailyIncrease` smallint,
+	`AvgIncrease` smallint,
+	primary key (`DayNum`)
+);
+
+drop table if exists `Crime`;
+create table if not exists `Crime`(
+	`RowID` mediumint,
+	`DayNum` smallint,
+	`CrimeDate` date,
+	`CrimeCode` varchar(4),
+	`WeaponID` tinyint,
+	`Gender` char,
+	`Age` tinyint,
+	`District` varchar(2),
+	`Latitude` float,
+	`Longitude` float,
+	primary key (`RowID`)
+);
