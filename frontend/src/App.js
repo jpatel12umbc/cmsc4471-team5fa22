f86f2393b2cb23296d086e95878808ebbf3ee11f
Dillon Ward <98842483+Dillon1271@users.noreply.github.com>
2022-11-14 16:53:43 -0500

    Added Criminal Age Bar Graph

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import React,{useRef, useEffect, useState} from 'react';
 import Axios from 'axios';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries} from "react-vis";
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries, VerticalBarSeries,VerticalBarSeriesCanvas} from "react-vis";
 
 
 function App(){
   
   //stores currently graphed covid weekly avg increase (the line graph)
   const [coviddata, setcoviddata] = useState([]);
-
   //stores start and end date respectively for covid line graph
   const [endDate, setEndDate] = useState('');
   const [startDate, setStartDate] = useState('');
 
 
+  //stores currently graphed criminal age (the age bar graph)
+  const [crimeagebar, setcrimeagebar] = useState([]);
+  //stores start and end date respectively for crime bargraph age (bga) and district
+  const [endDatebga, setEndDatebga] = useState('');
+  const [startDatebga, setStartDatebga] = useState('');
+  const [districtbga, setdistrictbga] = useState('');
 
+  
   //sends start and end date to backend, gets list of dictionaries back for line graph
   const startEndDate = () => {
     Axios.get('http://localhost:5000/covidlinegraph', {params:{
@@ -27,7 +33,20 @@ function App(){
   })
   }
 
-  //Gets all coviddata from backend as a list of dictionaries to be graphes
+  //sends start/end date and district to backend, gets list of dictionaries back for crime age bar graph. Overrides crimeagebar to update bar graph
+  const startEndDatebga = () => {
+    Axios.get('http://localhost:5000/crimeagebargraphupdate', {params:{
+      startdatebga: startDatebga, 
+      enddatebga: endDatebga,
+      districtbga: districtbga}
+    } ).then((response) => {
+      //Overrides stored data in crimeagebar variable
+      console.log(response.data)
+      setcrimeagebar(response.data)
+  })
+  }
+
+  //Gets all coviddata from backend as a list of dictionaries to be graphed
   useEffect(() => {
     fetch("http://localhost:5000/").then(
       res => res.json()
@@ -39,6 +58,20 @@ function App(){
     )
   },[]);
 
+  
+  //Gets default parametered crime data from backend as a list of dictionaries to be age bar graph (first 100 days, all districts) displays on page start
+  useEffect(() => {
+    fetch("http://localhost:5000/crimebargraph").then(
+      res => res.json()
+    ).then(
+      data => {
+        setcrimeagebar(data)
+        console.log(data)
+      }
+    )
+  },[]);
+
+
 
   return (
     <div className="App">
@@ -49,10 +82,9 @@ function App(){
       </header>
       
 
-
-
+      {/*Covid line graph#################################################################################################### */}
       <form className='dateform'>
-        
+      
         {/*Gets user input from calendar, stores in startDate variable*/}
         <label>Start Date:</label>
         <input required type="date" id="start" name="date-start" 
@@ -96,6 +128,71 @@ function App(){
 
         </XYPlot>
       </div>
+
+      {/*Crime age bar graph#################################################################################################### */}
+      <form className='dateform'>
+        
+        {/*Gets user input from calendar, stores in startDatebga variable*/}
+        <label>Start Date:</label>
+        <input required type="date" id="agestart" name="date-start" 
+        min="2020-03-15" max="2021-09-30"
+        onChange={(e) =>{
+          setStartDatebga(e.target.value)
+        }}></input>
+        
+        {/*Gets user input from calendar, stores in endDatebga variable*/}
+        <label>End Date:</label>
+        <input required type="date" id="ageend" name="date-end"
+        min="2020-03-15" max="2021-09-30"
+        onChange={(e) =>{
+          setEndDatebga(e.target.value)
+        }}></input>
+
+         {/*Gets user input from list for district, stores in districtbga variable*/}
+         <label>District:</label>
+        <select required type="" id="agedisctrict" name="district"
+        onChange={(e) =>{
+          setdistrictbga(e.target.value)
+        }}>
+          <option value="Al">All</option>
+          <option value="N">Northern</option>
+          <option value="S">Southern</option>
+          <option value="E">Eastern</option>
+          <option value="W">Western</option>
+          <option value="NE">NorthEastern</option>
+          <option value="NW">NorthWestern</option>
+          <option value="SE">SouthEaster</option>
+          <option value="SW">SouthWestern</option>
+          <option value="C">Central</option>
+        </select>
+
+        {/*Form submit button (KEEP AS TYPE button! breaks otherwise)*/ }
+        <input type="button" onClick={startEndDatebga} value="Submit"></input>
+      </form>
+      
+
+      {/*Crime bar graph display*/}
+      <div className='covidagebar'>
+
+        <h3 id="lgraphtitle">Criminal Ages </h3>
+        <link rel="stylesheet" href="https://unpkg.com/react-vis/dist/style.css"></link>
+
+        <XYPlot xType="ordinal" width={1000} height={300}>
+          <HorizontalGridLines />
+          <VerticalGridLines />
+          <XAxis  title="Ages"/>
+
+          <YAxis headLine title="Count" />
+
+          {/*Setting the data dictionary to be displayed. X is category, Y is #of occurences*/}  
+          <VerticalBarSeries data={crimeagebar} />
+
+        </XYPlot>
+      </div>
+
+
+
+
       
 
     {/* test output for covid cases for reference when returning query dump
